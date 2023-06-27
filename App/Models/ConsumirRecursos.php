e5e09c4de722ac7525f8ae76a4ee53a99d34be49
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2023-06-27 01:54:46 -0300

    Atualizando Saldos

--- App/Models/ConsumirRecursos.php
+++ App/Models/ConsumirRecursos.php
@@ -8,8 +8,12 @@ use App\models\Recurso;
 class ConsumirRecursos
 {
     private static $tableRecursos  = 'recursos';
+    private static $tableClubes  = 'clubes';
     public static $dadosRecurso = [];
     public static $dadosClube = [];
+
+    public static $valorRecursoAtualizado = 0;
+    public static $valorClubeAtualizado = 0;
 
     public static function consumir($dados)
     {
@@ -18,24 +22,53 @@ class ConsumirRecursos
         self::$dadosRecurso = Recurso::get_id($dados['recurso_id']);
         $valorCalculado = self::subtrair_recursos($dados['valor_consumo']);
 
-        $dadosTratados = [
-            "clube" => self::$dadosClube['nome_clube'],
-            "saldo_anterior" => self::$dadosClube['saldo_disponivel'],
-            "saldo_atual" => $valorCalculado,
-        ];
-        return $dadosTratados;
+
+        $responseAtt = self::ataulizarInformacoes($dados);
+        if ($responseAtt) {
+            $dadosTratados = [
+                "clube" => self::$dadosClube['nome_clube'],
+                "saldo_anterior" => self::$dadosClube['saldo_disponivel'],
+                "saldo_atual" => $valorCalculado,
+            ];
+            return $dadosTratados;
+        } else {
+            http_response_code(400);
+            return array('status' => 400, 'error' => 'Erro ao atualizar Saldo');
+        }
+    }
+
+    private static function ataulizarInformacoes($dados)
+    {
+        $connPdo = new \PDO(DBDRIVER . ':host=' . HOST . ';dbname=' . DBNAME, DBUSER, DBPASS);
+        try {
+            $connPdo->beginTransaction();
+
+            // Executa várias consultas SQL
+            $connPdo->exec("UPDATE " . self::$tableClubes . " SET saldo_disponivel = " . self::$valorClubeAtualizado . "  WHERE id =" . $dados['clube_id']);
+            $connPdo->exec("UPDATE " . self::$tableRecursos . " SET saldo_disponivel = " . self::$valorRecursoAtualizado . "  WHERE id = " . $dados['recurso_id']);
+
+            return $connPdo->commit();
+        } catch (\Exception $e) {
+            // Se ocorrer um erro, reverte a transação
+            $connPdo->rollBack();
+            echo "Falha: " . $e->getMessage();
+        }
     }
     private static function subtrair_recursos($valorConsumo)
     {
 
-        $valorRecursos = round(floatval(self::$dadosRecurso['saldo_disponível']), 2);
+        $valorRecursos = round(floatval(self::$dadosRecurso['saldo_disponivel']), 2);
         $valorClubes = round(floatval(self::$dadosClube['saldo_disponivel']), 2);
         $valorConsumo = str_replace(',', '.', $valorConsumo);
         $consumo = round(floatval($valorConsumo), 2);
 
         // Use o operador lógico correto "&&" em vez de "&"
         if ($valorRecursos >= $consumo && $valorClubes >= $consumo) {
+            $valorClubeAtualizadoDecimal = number_format($valorClubes - $consumo, 2);
+            $valorRecursoAtualizadoDecimal = number_format($valorRecursos - $consumo, 2);
             $valorClubeAtualizado = number_format($valorClubes - $consumo, 2);
+            self::$valorClubeAtualizado =  str_replace(',', '', $valorClubeAtualizadoDecimal);
+            self::$valorRecursoAtualizado =  str_replace(',', '', $valorRecursoAtualizadoDecimal);
             return floatval($valorClubeAtualizado);
         } else {
             if ($valorRecursos < $consumo) {
