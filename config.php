850e538a18030a174e7701accac067e8f7da2ebe
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2023-06-28 00:28:59 -0300

    Removendo acesso ao banco online

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     //MYSQL ALTERE CONFORME SEUS DADOS
     const DBDRIVER = 'mysql';
-    const HOST = 'mysql-133493-0.cloudclusters.net:19486';
+    const HOST = 'localhost';
     const DBNAME = 'gerenciar_recursos';
-    const DBUSER = 'admin';
-    const DBPASS = '1br5fzoD';
+    const DBUSER = 'root';
+    const DBPASS = '';
